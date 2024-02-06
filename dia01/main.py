81a773017771e22cb460298cd6d11a40c98bbad8
Juan Manuel Herrera Carrera <tampiko@gmail.com>
2024-02-06 10:29:12 -0600

    Dia 01

---
+++ dia01/main.py
@@ -0,0 +1,11 @@
+print('--------------------------------------------------------------------------')
+print('--------------------------------------------------------------------------')
+print('Bienvenido al sitio, para suscribirte, proporciona los siguientes datos:')
+print('');
+nombre    =input('Nombre: ')
+apellidos =input('Apellidos: ')
+correo    =input('Correo Electrónico: ')
+print('')
+print("Hola "+nombre+", en breve recibirás un correo a "+correo+".");
+print('--------------------------------------------------------------------------')
+print('--------------------------------------------------------------------------')
